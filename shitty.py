d7e308b61cde7199943ebbbe0c340f26d59059b4
Rodrigo <rodrigoqyt@gmail.com>
2023-04-29 23:28:55 -0600

    Update shitty.py

--- shitty.py
+++ shitty.py
@@ -5,26 +5,26 @@ import sys
 class Shitty(cmd.Cmd):
     prompt = 'shitty> '
 
-    def do_ls(self, args):
+    def do_get(self, args):
         files = os.listdir('.')
         for file in files:
             print(file)
 
-    def do_mkdir(self, directory):
+    def do_make(self, directory):
         try:
             os.mkdir(directory)
             print(f"Directory {directory} created successfully.")
         except OSError as error:
             print(f"Failed to create directory {directory}. Error: {error}")
 
-    def do_rmdir(self, directory):
+    def do_del(self, directory):
         try:
             os.rmdir(directory)
             print(f"Directory {directory} deleted successfully.")
         except OSError as error:
             print(f"Failed to delete directory {directory}. Error: {error}")
 
-    def do_touch(self, filename):
+    def do_file(self, filename):
         try:
             with open(filename, 'a'):
                 os.utime(filename, None)
